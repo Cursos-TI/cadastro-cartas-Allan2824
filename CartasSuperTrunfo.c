7d8055a28263bffa41dc8276635b199455df27f6
Allan2824 <allan-david@hotmail.com>
2025-12-04 23:45:25 +0000

    finalizado CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,35 +7,85 @@
 int main() {
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char carta1;
-  char Estado1[] = 'A';
-  char condigo_da_carta1[20] = 'A01';
-  char nome_da_cidade1[20] = "Brasilia";
-  int população1 = 1558000;
+  int carta1 = 1;
+  char Estado1[20] = "Acre";
+  char condigo_da_carta1[20] = "A01";
+  char nome_da_cidade1[20] = "Jordão";
+  int população1 = 198350;
   float area1 = 12550.90;
   float pib1 = 550.50;
-  int numero_de_pontos_turisticos1 = 5;
+  int numero_de_pontos_turisticos1 = 15;
  
-  char carta2;
-  char Estado2[] = 'B';
-  char condigo_da_carta2[20] = 'B02';
-  char nome_da_cidade2[20] = "Fortaleza";
+  int carta2 = 2;
+  char Estado2[] = "Bahia";
+  char condigo_da_carta2[20] = "B02";
+  char nome_da_cidade2[20] = "Boquira";
   int população2 = 2055010;
   float area2 = 21060.20;
   float pib2 = 300.70;
-  int numero_de_pontos_turisticos2 = 15;
+  int numero_de_pontos_turisticos2 = 5;
 
+
+  // area para variavel de entrada de dados
+  int jogador1;
+ 
+  int jogador2;
+  
+  char opcao[20];
+  
+  
   // Área para entrada de dados
-  printf ("Escolha uma carta, 1 ou 2: ");
-  scanf ("%s", carta1);
+  printf("JOGADOR 1, Escolha uma carta, 1 ou 2: \n");
+  scanf("%d", &jogador1);
+  
 
-  printf ("Agora escolha outra carta diferente da anterior: ");
-  scanf ("%s", carta2);
+  printf("JOGADOR 2, Digite o numero oposto do seu adversario: \n");
+  scanf("%d", &jogador2);
+
+  printf("Agora vocês devem escolher em acordo entre; POPULAÇÃO, AREA, BIP OU TURISMO: \n");
+  scanf("%s", opcao);
+  printf("\n");
+  printf("\n");
+
+  
+  // Area para exibiçao da escolha dos jogadores
+  printf("Primeiro Jogador, Carta: %d\n", jogador1);
+  
+  printf("Segundo Jogador, Carta: %d\n", jogador2);
+  printf("\n");
+  printf("\n");
+  
+  printf("IREMOS COMPARAR: %s", opcao);
+  printf("\n");
+  printf("\n");
+
 
   // Área para exibição dos dados da cidade
   
- 
+  printf("carta: %d\n", carta1);
+  printf("estado: %s\n", Estado1);
+  printf("codigo: %s\n", condigo_da_carta1);
+  printf("Cidade: %s\n", nome_da_cidade1);
+  printf("População: %i Milhões de pessoas\n",população1);
+  printf("Area: %.2f km²\n", area1);
+  printf("Pib: %.2f Bilhões\n", pib1),
+  printf("Ponto turisticos: %d\n", numero_de_pontos_turisticos1);
+  printf("\n");
 
-  
+  printf("carta: %d\n", carta2);
+  printf("estado: %s\n", Estado2);
+  printf("codigo: %s\n", condigo_da_carta2);
+  printf("Cidade: %s\n", nome_da_cidade2);
+  printf("População: %i Milhões de pessoas\n",população2);
+  printf("Area: %.2f km²\n", area2);
+  printf("Pib: %.2f Bilhões\n", pib2),
+  printf("Ponto turisticos: %d\n", numero_de_pontos_turisticos2);
+  printf("\n");
+
+
+ // area para consideraçoes finais
+ printf("CONFIRA, O JOGADOR QUE TEM A PONTUACAO MAIOR NA SUA CARTA, GANHOU!\n");
+ 
+ 
 return 0;
-} 
+}
