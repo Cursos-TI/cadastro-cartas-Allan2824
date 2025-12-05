2bbe52b680877086a2304e6f8eb0a9490f57b07a
Allan2824 <allan-david@hotmail.com>
2025-12-05 23:55:33 +0000

    atribuição de operações de divisão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,16 +13,16 @@ int main() {
   char nome_da_cidade1[20] = "Jordão";
   int população1 = 198350;
   float area1 = 12550.90;
-  float pib1 = 550.50;
+  float pib1 = 550890.50;
   int numero_de_pontos_turisticos1 = 15;
  
   int carta2 = 2;
   char Estado2[] = "Bahia";
   char condigo_da_carta2[20] = "B02";
   char nome_da_cidade2[20] = "Boquira";
-  int população2 = 2055010;
+  int população2 = 205010;
   float area2 = 21060.20;
-  float pib2 = 300.70;
+  float pib2 = 33550.70;
   int numero_de_pontos_turisticos2 = 5;
 
 
@@ -34,15 +34,22 @@ int main() {
   char opcao[20];
   
   
+ //area para operações
+  float densidade_populacional1 = população1/area1;
+  float pib_per_capita1 = pib1/população1;
+
+  float densidade_populacional2 = população2/area2;
+  float pib_per_capita2 = pib2/população2;
+
+
   // Área para entrada de dados
   printf("JOGADOR 1, Escolha uma carta, 1 ou 2: \n");
   scanf("%d", &jogador1);
   
-
   printf("JOGADOR 2, Digite o numero oposto do seu adversario: \n");
   scanf("%d", &jogador2);
 
-  printf("Agora vocês devem escolher em acordo entre; POPULAÇÃO, AREA, BIP OU TURISMO: \n");
+  printf("Agora vocês devem escolher em acordo entre; POPULAÇÃO, AREA, PIB OU TURISMO: \n");
   scanf("%s", opcao);
   printf("\n");
   printf("\n");
@@ -70,6 +77,8 @@ int main() {
   printf("Area: %.2f km²\n", area1);
   printf("Pib: %.2f Bilhões\n", pib1),
   printf("Ponto turisticos: %d\n", numero_de_pontos_turisticos1);
+  printf("Densidade Populacional: %.2f hab/km² \n", densidade_populacional1);
+  printf("Pib per Capita: %.2f reais\n", pib_per_capita1);
   printf("\n");
 
   printf("carta: %d\n", carta2);
@@ -80,6 +89,8 @@ int main() {
   printf("Area: %.2f km²\n", area2);
   printf("Pib: %.2f Bilhões\n", pib2),
   printf("Ponto turisticos: %d\n", numero_de_pontos_turisticos2);
+  printf("Densidade Populacional: %2f hab/km²\n", densidade_populacional2);
+  printf("Pib per Capita: %.2f reais\n", pib_per_capita2);
   printf("\n");
 
 
